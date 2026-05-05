1940193592b48d1626ebc0b8456b65416bdd72bd
pedro ndong <pndong55@gmail.com>
2026-05-05 20:19:57 +0100

    ci: pipeline CI/CD completo con DOCR y Terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,10 +13,14 @@ terraform {
     bucket                      = "netshield-tf-state"
     key                         = "prod/terraform.tfstate"
     region                      = "us-east-1"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    force_path_style            = true
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+    skip_credentials_validation  = true
+    skip_metadata_api_check      = true
+    skip_region_validation       = true
+    skip_requesting_account_id   = true
+    use_path_style               = true
   }
 }
 
